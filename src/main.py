42a199784fd8a7dd1df73aa065729b7a0d3fea66
Alex Emelyanov <alex84_84_84@mail.ru>
2020-12-25 19:33:59 +0300

    add func for det and zip download

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-import tarfile
+import zipfile
 import cv2
 from numpy import random
 import supervisely_lib as sly
@@ -19,7 +19,7 @@ ARH_NAME = os.environ.get("modal.state.slyArh")
 
 
 
-def get_frames_with_objects(txt_path):
+def get_frames_with_objects_gt(txt_path):
     frame_to_objects = defaultdict(lambda: defaultdict(list))
     with open(txt_path, "r") as file:
         all_lines = file.readlines()
@@ -28,6 +28,18 @@ def get_frames_with_objects(txt_path):
             line = list(map(int, line))
             frame_to_objects[line[0]][line[1]].extend(line[2:6])
     return frame_to_objects
+
+
+def get_frames_with_objects_det(txt_path):
+    frame_to_objects = defaultdict(list)
+    with open(txt_path, "r") as file:
+        all_lines = file.readlines()
+        for line in all_lines:
+            line = line.split('\n')[0].split(',')[:-4]
+            line = list(map(int, line))
+            frame_to_objects[line[0]].extend([line[2:6]])
+    return frame_to_objects
+
 
 
 @my_app.callback("import_mot_format")
@@ -42,39 +54,45 @@ def import_mot_format(api: sly.Api, task_id, context, state, app_logger):
         cur_files_path = INPUT_FILE
         archive_path = storage_dir + cur_files_path
 
-    api.file.download(TEAM_ID, cur_files_path, archive_path)
-
-    if tarfile.is_tarfile(archive_path):
-        with tarfile.open(archive_path) as archive:
-            archive.extractall(storage_dir)
-    else:
-        raise Exception("No such file".format(ARH_NAME))
-
+    #api.file.download(TEAM_ID, cur_files_path, archive_path)
+    #if zipfile.is_zipfile(archive_path):
+        #with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+            #zip_ref.extractall(storage_dir)
+    #else:
+        #raise Exception("No such file".format(ARH_NAME))
 
     #new_project = api.project.create(WORKSPACE_ID, 'mot', change_name_if_conflict=True)
-    txt_path = '/home/andrew/alex_work/app/import-mot-format/MOT20Labels/train/MOT20-01/gt/gt.txt'
-    test_path = '/home/andrew/alex_work/app/import-mot-format/MOT20Labels'
+
+    #test_gt = '/home/andrew/alex_work/app_data/data/MOT20/train/MOT20-05/gt/gt.txt'
+    #test_det = '/home/andrew/alex_work/app_data/data/MOT20/train/MOT20-05/det/det.txt'
+    #gt = get_frames_with_objects_gt(test_gt)
+    #det = get_frames_with_objects_det(test_det)
+
+    test_path = '/home/andrew/alex_work/app_data/data/MOT20'
     img_size = (1000, 1000)
     for r, d, f in os.walk(test_path):
-        if f == ['gt.txt']:
+        if f == ['det.txt']:
             new_dataset_name = r.split('/')[-2]
+            imgs_path = r[:-3] + 'img1'
             #new_dataset = api.dataset.create(new_project.id, new_dataset_name, change_name_if_conflict=True)
             res_anns = []
-            frames_with_objects = get_frames_with_objects(os.path.join(r, f[0]))
+            frames_with_objects = get_frames_with_objects_det(os.path.join(r, f[0]))
             for frame in frames_with_objects:
+                img_name = str(frame).zfill(6) + '.jpg'
+                img_path = os.path.join(imgs_path, img_name)
                 labels = []
-                for curr_obj in frames_with_objects[frame]:
-                    coords = frames_with_objects[frame][curr_obj]
-                    geom = sly.Rectangle(coords[1], coords[0], coords[1] + coords[3], coords[0] + coords[2])
+                coords = frames_with_objects[frame]
+                for curr_coord in coords:
+                    geom = sly.Rectangle(curr_coord[1], curr_coord[0], curr_coord[1] + curr_coord[3], curr_coord[0] + curr_coord[2])
                     obj_class = sly.ObjClass('pedestrain', sly.Rectangle)
                     label = sly.Label(geom, obj_class)
                     labels.append(label)
                 ann = sly.Annotation(img_size, labels)
                 res_anns.append(ann)
 
-            new_image_infos = api.image.upload_nps(new_dataset.id, res_images_names, res_images)
-            image_ids = [img_info.id for img_info in new_image_infos]
-            api.annotation.upload_anns(image_ids, res_anns)
+            #new_image_infos = api.image.upload_nps(new_dataset.id, res_images_names, res_images)
+            #image_ids = [img_info.id for img_info in new_image_infos]
+            #api.annotation.upload_anns(image_ids, res_anns)
 
 
     #progress = sly.Progress('Start detecting...', 1)
