86a8234b98f1bf36e3af5a3c63ab6a7a13761647
Alex Emelyanov <alex84_84_84@mail.ru>
2020-12-28 18:17:37 +0300

    mot to video format ready

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import os
 import zipfile
 import cv2
-from numpy import random
 import supervisely_lib as sly
 from supervisely_lib.annotation.tag_meta import TagValueType
-import numpy as np
 from collections import defaultdict
+
 
 
 my_app = sly.AppService()
@@ -17,7 +16,14 @@ INPUT_FOLDER = os.environ.get("modal.state.slyFolder")
 INPUT_FILE = os.environ.get("modal.state.slyFile")
 ARH_NAME = os.environ.get("modal.state.slyArh")
 
-obj_class_name = 'pedestrain'
+obj_class_name = 'pedestrian'
+project_name = 'mot_video'
+video_ext = '.mp4'
+dataset_name = 'ds1'
+mot_bbox_file_name = 'gt.txt'
+seqinfo_file_name = 'seqinfo.ini'
+frame_rate_default = 25
+image_name_length = 6
 logger = sly.logger
 
 
@@ -25,24 +31,26 @@ def check_mot_format(input_dir):
     possible_images_extentions = set(['jpg', 'jpeg', 'mpo', 'bmp', 'png', 'webp'])
     mot_datasets = 0
     for r, d, f in os.walk(input_dir):
-        if 'img1' in d and 'det' in d:
-            check_det = os.listdir(r + '/det')
-            if 'det.txt' not in check_det:
-                raise ValueError('Folder {} should contain det.txt file'.format(r + '/det'))
+        if 'img1' in d and 'gt' in d:
+            if seqinfo_file_name not in f:
+                logger.warning('Folder {} should contain seqinfo.ini file, will be used frame_rate by default'.format(r))
+            check_det = os.listdir(r + '/gt')
+            if mot_bbox_file_name not in check_det:
+                raise ValueError('Folder {} should contain gt.txt file'.format(r + '/gt'))
             check_imgs = os.listdir(r + '/img1')
             files_exts = set([file.split('.')[1] for file in check_imgs])
             img_names_length = set([len(file.split('.')[0]) for file in check_imgs])
             img_exts = list(files_exts & possible_images_extentions)
             if len(img_exts) == 0:
                 raise ValueError('Where is no images in {} directory'.format(r + '/img1'))
-            if 6 not in img_names_length or len(img_names_length) > 1:
-                raise ValueError('Image name should have length 6 like: 000001.jpg, 000002.jpg')
+            if image_name_length not in img_names_length or len(img_names_length) > 1:
+                raise ValueError('Image name should have length {} like: 000001.jpg, 000002.jpg'.format(image_name_length))
             over_files = files_exts - possible_images_extentions
             if len(over_files) > 0:
                 raise ValueError('Folder {} should contain only images, not files with {} extentions'.format(r + '/img1', over_files))
             mot_datasets += 1
     if mot_datasets == 0:
-        raise ValueError('Input format of data does not match the required MOT dataset structure: mot_folder:det_dir, img1_dir')
+        raise ValueError('Input format of data does not match the required MOT dataset structure: mot_folder:gt_dir, img1_dir')
 
 
 def get_frames_with_objects_gt(txt_path):
@@ -51,6 +59,9 @@ def get_frames_with_objects_gt(txt_path):
         all_lines = file.readlines()
         for line in all_lines:
             line = line.split('\n')[0].split(',')[:-3]
+            if len(line) == 0:
+                continue
+            line = list(map(lambda x: round(float(x)), line))
             line = list(map(int, line))
             frame_to_objects[line[0]][line[1]].extend(line[2:6])
     return frame_to_objects
@@ -70,12 +81,13 @@ def get_frames_with_objects_det(txt_path):
     return frame_to_objects
 
 
-#def img_size_from_seqini(txt_path):
-#    with open(txt_path, "r") as file:
-#        all_lines = file.readlines()
-#        img_width = int(all_lines[5].split('\n')[0].split('=')[1])
-#        img_height = int(all_lines[6].split('\n')[0].split('=')[1])
-#    return (img_width, img_height)
+def img_size_from_seqini(txt_path):
+    with open(txt_path, "r") as file:
+        all_lines = file.readlines()
+        img_width = int(all_lines[5].split('\n')[0].split('=')[1])
+        img_height = int(all_lines[6].split('\n')[0].split('=')[1])
+        frame_rate = int(all_lines[3].split('\n')[0].split('=')[1])
+    return (img_width, img_height), frame_rate
 
 
 @my_app.callback("import_mot_format")
@@ -90,60 +102,75 @@ def import_mot_format(api: sly.Api, task_id, context, state, app_logger):
         raise ValueError('Input folder not exist')
 
     logger.info('Download archive')
-    #api.file.download(TEAM_ID, cur_files_path, archive_path)
-    #if zipfile.is_zipfile(archive_path):
-    #    logger.info('Extract archive')
-    #    with zipfile.ZipFile(archive_path, 'r') as zip_ref:
-    #        zip_ref.extractall(storage_dir)
-    #else:
-    #    raise Exception("No such file".format(ARH_NAME))
+    api.file.download(TEAM_ID, cur_files_path, archive_path)
+    if zipfile.is_zipfile(archive_path):
+        logger.info('Extract archive')
+        with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+            zip_ref.extractall(storage_dir)
+    else:
+        raise Exception("No such file".format(ARH_NAME))
 
     logger.info('Check input mot format')
     check_mot_format(storage_dir)
-    new_project = api.project.create(WORKSPACE_ID, 'mot', change_name_if_conflict=True)
+    new_project = api.project.create(WORKSPACE_ID, project_name, type=sly.ProjectType.VIDEOS, change_name_if_conflict=True)
     obj_class = sly.ObjClass(obj_class_name, sly.Rectangle)
     meta = sly.ProjectMeta(sly.ObjClassCollection([obj_class]))
     api.project.update_meta(new_project.id, meta.to_json())
+    new_dataset = api.dataset.create(new_project.id, dataset_name, change_name_if_conflict=True)
     for r, d, f in os.walk(storage_dir):
-        if 'det.txt' in f:
-            new_dataset_name = r.split('/')[-2]
-            logger.info('Dataset with name {} in processing'.format(new_dataset_name))
-            imgs_path = r[:-3] + 'img1'
-            img_to_find_shape = os.listdir(imgs_path)[0]
-            img = sly.imaging.image.read(os.path.join(imgs_path, img_to_find_shape))
-            img_shape = (img.shape[0], img.shape[1])
-            #seqinfo_path = r[:-3] + 'seqinfo.ini'
-            #img_size = img_size_from_seqini(seqinfo_path)
-            new_dataset = api.dataset.create(new_project.id, new_dataset_name, change_name_if_conflict=True)
-            frames_with_objects = get_frames_with_objects_det(os.path.join(r, 'det.txt'))
+        if mot_bbox_file_name in f:
+            video_name = r.split('/')[-2] + video_ext
+            logger.info('Video {} being processed'.format(video_name))
+            video_path = os.path.join(storage_dir, video_name)
+            imgs_path = r[:-2] + 'img1'
             images = os.listdir(imgs_path)
-            #to_debag = 0 # TODO del comment to test project fast
-            for batch in sly.batched(images):
-                #if to_debag == 1: # TODO del comment to test project fast
-                    #break
-                res_anns = []
-                full_images_paths = []
-                for img_name in batch:
-                    full_images_paths.append(os.path.join(imgs_path, img_name))
-                    frame = int(img_name.split('.')[0])
-                    labels = []
-                    coords = frames_with_objects[frame]
-                    if len(coords) == 0:
-                        logger.warning('Where is no data for image {} in det.txt'.format(img_name))
-                        ann = sly.Annotation(img_shape)
-                    else:
-                        for curr_coord in coords:
-                            geom = sly.Rectangle(curr_coord[1], curr_coord[0], curr_coord[1] + curr_coord[3], curr_coord[0] + curr_coord[2])
-                            label = sly.Label(geom, obj_class)
-                            labels.append(label)
-                        ann = sly.Annotation(img_shape, labels)
-                    res_anns.append(ann)
-                new_image_infos = api.image.upload_paths(new_dataset.id, batch, full_images_paths)
-                image_ids = [img_info.id for img_info in new_image_infos]
-                api.annotation.upload_anns(image_ids, res_anns)
-                #to_debag += 1 # TODO del comment to test project fast
-            logger.info('Dataset with name {} upload with {} images'.format(new_dataset_name, len(images)))
+            images_ext = images[0].split('.')[1]
+            seqinfo_path = r[:-2] + seqinfo_file_name
+            frames_with_objects = get_frames_with_objects_gt(os.path.join(r, mot_bbox_file_name))
+            if os.path.isfile(seqinfo_path):
+                img_size, frame_rate = img_size_from_seqini(seqinfo_path)
+            else:
+                img = sly.imaging.image.read(os.path.join(imgs_path, images[0]))
+                img_size = (img.shape[1], img.shape[0])
+                frame_rate = frame_rate_default
+            video = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'MP4V'), frame_rate, img_size)
+            im_names = []
+            im_paths = []
+            ids_to_video_object = {}
+            new_frames = []
+            for image_id in range(1, len(images) + 1):
+                new_figures = []
+                image_name = str(image_id).zfill(image_name_length) + '.' + images_ext
+                im_names.append(image_name)
+                im_paths.append(os.path.join(imgs_path, image_name))
+                video.write(cv2.imread(os.path.join(imgs_path, image_name)))
+                frame_object_coords = frames_with_objects[image_id]
+                for idx, coords in frame_object_coords.items():
+                    if idx not in ids_to_video_object.keys():
+                        ids_to_video_object[idx] = sly.VideoObject(obj_class)
+                    left, top, w, h = coords
+                    bottom = top + h
+                    if round(bottom) >= img_size[1]:
+                        bottom = img_size[1] - 1
+                    right = left + w
+                    if round(right) >= img_size[0]:
+                        right = img_size[0] - 1
+                    if left < 0:
+                        left = 0
+                    if top < 0:
+                        top = 0
+                    geom = sly.Rectangle(top, left, bottom, right)
+                    figure = sly.VideoFigure(ids_to_video_object[idx], geom, image_id - 1)
+                    new_figures.append(figure)
+                new_frame = sly.Frame(image_id - 1, new_figures)
+                new_frames.append(new_frame)
 
+            video.release()
+            file_info = api.video.upload_paths(new_dataset.id, [video_name], [video_path])
+            new_frames_collection = sly.FrameCollection(new_frames)
+            new_objects = sly.VideoObjectCollection(ids_to_video_object.values())
+            ann = sly.VideoAnnotation((img_size[1], img_size[0]), len(new_frames), objects=new_objects, frames=new_frames_collection)
+            api.video.annotation.append(file_info[0].id, ann)
     my_app.stop()
 
 
