85d14b2e5e4bc8d10b5fbedb69935c9bd4308d4a
Alex Emelyanov <alex84_84_84@mail.ru>
2021-06-04 17:43:39 +0300

    type(mot_ds_names_str)

--- src/main.py
+++ src/main.py
@@ -35,6 +35,7 @@ if mot_dataset == custom_ds:
 else:
     mot_ds_names_str = os.environ['modal.state.currDatasets']
     logger.warn('{}'.format(mot_ds_names_str))
+    logger.warn('{}'.format(type(mot_ds_names_str)))
     mot_ds_names = mot_ds_names_str.replace('\'', '')
     mot_ds_names = mot_ds_names.replace(' ', '')
     mot_ds_names = mot_ds_names[1:-1].split(',')
