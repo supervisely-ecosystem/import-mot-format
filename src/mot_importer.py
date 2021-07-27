5638bdd7dd818299ff095bc3cbabfd15930c0f63
cxnt <pavelbartsits@gmail.com>
2021-07-27 16:49:05 +0300

    tasks workspace

--- src/mot_importer.py
+++ src/mot_importer.py
@@ -80,18 +80,13 @@ def import_dataset(new_project, ds_name, curr_mot_dir, meta, conf_tag_meta, app_
     obj_classes = []
     obj_class_names = []
     new_dataset = g.api.dataset.create(new_project.id, ds_name, change_name_if_conflict=True)
-
-    video_names = [d for d in os.listdir(curr_mot_dir) if os.path.isdir(os.path.join(curr_mot_dir, d))]
-
-    progress = sly.Progress(f'Importing videos', len(video_names), app_logger)
     for r, d, f in os.walk(curr_mot_dir):
         if r.split('/')[-1] == g.mot_bbox_filename:
             video_name = r.split('/')[-2] + g.video_ext
             video_path = os.path.join(curr_mot_dir, video_name)
             imgs_path = r[:-2] + 'img1'
             images = os.listdir(imgs_path)
-            # progress = sly.Progress(f'Importing "{video_name}"', len(images), app_logger)
-            processed_video_frames = 0
+            progress = sly.Progress(f'Importing "{video_name}"', len(images), app_logger)
             images_ext = images[0].split('.')[1]
             seqinfo_path = r[:-2] + g.seqinfo_file_name
             if os.path.isfile(seqinfo_path):
@@ -165,10 +160,8 @@ def import_dataset(new_project, ds_name, curr_mot_dir, meta, conf_tag_meta, app_
 
                 new_frame = sly.Frame(image_id - 1, new_figures)
                 new_frames.append(new_frame)
-                processed_video_frames += 1
-                g.logger.info(f"Processing video frames: {processed_video_frames}/{len(images)}")
+                progress.iter_done_report()
 
-            progress.iter_done_report()
             video.release()
 
             video_objects = []
