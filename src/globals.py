e17c60eeacc9518c92b2c3ab7cfa63f755772350
cxnt <pavelbartsits@gmail.com>
2021-07-27 15:44:50 +0300

    appear progress in app logs

--- src/globals.py
+++ src/globals.py
@@ -5,9 +5,6 @@ import supervisely_lib as sly
 my_app = sly.AppService()
 api: sly.Api = my_app.public_api
 
-
-my_app.logger()
-
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 
